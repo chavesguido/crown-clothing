38b3de56b9ac2879192c231fce02938ba1a13acd
gchaves_alba <gchaves@albacaucion.com>
2019-10-21 12:46:32 -0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -40,7 +40,7 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
     currentUser,
     hidden
 });
